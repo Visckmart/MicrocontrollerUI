1b84c78c4007650bc4cf2d6b1fdb1e9abf6bb92f
Visckmart <visckmart@hotmail.com>
2019-03-02 21:49:46 -0300

    Initial Commit

---
+++ Node_Communication/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Node_Communication
+//
+//  Created by Victor Martins on 02/03/19.
+//  Copyright © 2019 Visckmart. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Node_Communication/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Node_Communication
+//
+//  Created by Victor Martins on 02/03/19.
+//  Copyright © 2019 Visckmart. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
